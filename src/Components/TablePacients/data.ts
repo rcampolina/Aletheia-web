81f8defd34fd8b5a694ba29ecbff46e65fe7f021
Rafael Campolina <rafaelcampolina@outlook.com.br>
2021-08-29 14:37:35 -0300

    Tabela de pacientes funcinando, atualização de pacotes, remoção de arquivos desncessários.

--- src/Components/TablePacients/data.ts
+++
@@ -1,9 +0,0 @@
-import api from "../../services/api";
-
-async function dataTable() {
-  const resultado = await api.get('users').then(res => res.data);
-  console.log(resultado)
-  return resultado;
-}
-
-export default dataTable;
--- src/Components/TablePacients/index.jsx
+++ src/Components/TablePacients/index.jsx
@@ -1,45 +1,191 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 import api from '../../services/api';
+import { useTable, usePagination, useRowSelect } from 'react-table'
+import styled from 'styled-components'
 
 import "./styles.css";
 
-function confirmAttendance(id) {
-  console.log('Confirmado: ' + id)
+const Styles = styled.div`
+  padding: 1rem;
+
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+
+  .pagination {
+    padding: 0.5rem;
+  }
+`
+
+const IndeterminateCheckbox = React.forwardRef(
+  ({ indeterminate, ...rest }, ref) => {
+    const defaultRef = React.useRef()
+    const resolvedRef = ref || defaultRef
+
+    React.useEffect(() => {
+      resolvedRef.current.indeterminate = indeterminate
+    }, [resolvedRef, indeterminate])
+
+    return (
+      <>
+        <input type="checkbox" ref={resolvedRef} {...rest} />
+      </>
+    )
+  }
+)
+
+function Table({ columns, data }) {
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page, // Instead of using 'rows', we'll use page,
+    // which has only the rows for the active page
+
+    // The rest of these things are super handy, too ;)
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    selectedFlatRows,
+    state: { pageIndex, pageSize, selectedRowIds },
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    usePagination,
+    useRowSelect,
+    hooks => {
+      hooks.visibleColumns.push(columns => [
+        // Let's make a column for selection
+        {
+          id: 'selection',
+          // The header can use the table's getToggleAllRowsSelectedProps method
+          // to render a checkbox
+          Header: ({ getToggleAllPageRowsSelectedProps }) => (
+            <div>
+              <IndeterminateCheckbox {...getToggleAllPageRowsSelectedProps()} />
+            </div>
+          ),
+          // The cell can use the individual row's getToggleRowSelectedProps method
+          // to the render a checkbox
+          Cell: ({ row }) => (
+            <div>
+              <IndeterminateCheckbox {...row.getToggleRowSelectedProps()} />
+            </div>
+          ),
+        },
+        ...columns,
+      ])
+    }
+  )
+
+  // Render the UI for your table
+  return (
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row, i) => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      {/* 
+        Pagination can be built however you'd like. 
+        This is just a very basic UI implementation:
+      */}
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(page)
+            }}
+            style={{ width: '100px' }}
+          />
+        </span>{' '}
+        <select
+          value={pageSize}
+          onChange={e => {
+            setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
+  )
 }
-
-const HeaderData = ({ data }) => {
-  const keys = Object.keys(data[0]);
-
-  return (
-    <thead>
-      <tr>
-        {keys.map((key) => (
-          <th key={key}>{key}</th>
-        ))}
-      </tr>
-    </thead>
-  );
-};
-
-const BodyData = ({ data }) => {
-  const keys = Object.keys(data[0]);
-
-  return (
-    <tbody>
-      {data.map((dados) => {
-        return (
-          <tr key={dados.id}>
-            <button onClick={confirmAttendance(dados.id)} >Confirmar</button>
-            {keys.map((key) => (
-              <td key={key}>{dados[key]}</td>
-            ))}
-          </tr>
-        );
-      })}
-    </tbody>
-  );
-};
 
 const TablePacients = () => {
   const [data, setData] = useState([{}]);
@@ -48,16 +194,70 @@ const TablePacients = () => {
     (async () => {
       const res = await api.get('users');
       setData(res.data);
-      console.log(data);
+      console.log(res.data);
     })()
   }, [])
 
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Pacientes',
+        columns: [
+          {
+            Header: 'Nome',
+            accessor: 'name',
+          },
+          {
+            Header: 'Email',
+            accessor: 'email',
+          },
+          {
+            Header: 'phone',
+            accessor: 'phone',
+          },
+          {
+            Header: 'whatsapp',
+            accessor: 'whatsapp',
+          },
+          {
+            Header: 'CPF',
+            accessor: 'cpf',
+          },
+          {
+            Header: 'Aniversário',
+            accessor: 'birthDate',
+          },
+          {
+            Header: 'Endereço',
+            accessor: 'adress',
+          },
+          {
+            Header: 'Condição',
+            accessor: 'condition',
+          },
+          {
+            Header: 'Observação',
+            accessor: 'observations',
+          },
+          {
+            Header: 'Próximo Atendimento',
+            accessor: 'next_attendance_date',
+          },
+          {
+            Header: 'Primeiro Atendimento',
+            accessor: 'first_attendance',
+          },
+        ],
+      },
+    ],
+    []
+  )
+
   return (
-    <table className="container-table bordered highlight responsive-table">
-      <HeaderData data={data} />
-      <BodyData data={data} />
-    </table>
-  );
+    <Styles>
+      <Table className="container" columns={columns} data={data} />
+    </Styles>
+  )
 };
 
 export default TablePacients;
